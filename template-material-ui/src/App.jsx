dc40e93f50c4ceb058f594b2c7d1402133c7b5ee
Muhammad Ali Khalil <mdalikhalil20@gmail.com>
2025-03-12 13:54:50 +0500

    Sign-In Template

--- template-material-ui/src/App.jsx
+++ template-material-ui/src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import SignInSide from './SignInSide';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
+      <SignInSide />
     </>
   )
 }
